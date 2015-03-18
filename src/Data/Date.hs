fa23ccb12e245c743d564e37a36f8da22ae68fce
Isaac Elliott <isaace71295@gmail.com>
2015-03-18 22:04:41 +1000

    Implemented date parsing
    readDate takes a format string and an input string. It parses the
    input string according the the format string and tries to return
    a date.

--- src/Data/Date.hs
+++ src/Data/Date.hs
@@ -4,6 +4,7 @@ module Data.Date(
     , month
     , year
     , mkDate
+    , readDate
     , showDate
     , difference
 ) where
--- src/Data/Date/Parsing.hs
+++ src/Data/Date/Parsing.hs
@@ -1,7 +1,11 @@
-module Data.Date.Parsing(showDate) where
+module Data.Date.Parsing(
+    readDate
+    , showDate
+) where
 
 import Data.Date.Internals
 
+import Data.Char (toLower)
 import Text.Parsec hiding (token,tokens)
 import Text.Parsec.String
 
@@ -28,10 +32,79 @@ import Text.Parsec.String
 -- >>> showDate "%D, %o of %M, %Y" $ Date { year = 2015, month = 2, day = 22 }
 -- Right "Sunday, 22nd of February, 2015"
 showDate :: String -> Date -> Either ParseError String
-showDate s d = 
-    case parseTokens s  of
+showDate fmt d = 
+    case tokenize fmt  of
         Right ts -> Right $ concatMap (showToken d) ts
         Left e   -> Left e
+
+readDate :: String -> String -> Either ParseError (Maybe Date)
+readDate fmt s =
+    case tokenize fmt of
+        Right ts -> parseDate ts s
+        Left e   -> Left e
+
+data DateState = DateState { 
+    yearState :: Maybe Integer
+    , monthState :: Maybe Int
+    , dayState :: Maybe Int
+}
+
+readToken :: Token -> DateState -> Parser DateState
+readToken (DayNumber) ds = do
+    d <- try (count 2 digit) <|> count 1 digit
+    return ds { dayState = Just (read d) }
+
+readToken (DayOrdinal) ds = do
+    d <- try (count 2 digit) <|> count 1 digit
+    string "st" <|> string "nd" <|> string "rd" <|> string "th"
+    return ds { dayState = Just (read d) }
+
+readToken (MonthNumber) ds = do
+    m <- try (count 2 digit) <|> count 1 digit
+    return ds { monthState = Just (read m) }
+
+readToken (MonthName) ds = do
+    m <- many1 letter
+    return ds { monthState = case map toLower m of
+        "january"   -> Just 1
+        "february"  -> Just 2
+        "march"     -> Just 3
+        "april"     -> Just 4
+        "may"       -> Just 5
+        "june"      -> Just 6
+        "july"      -> Just 7
+        "august"    -> Just 8
+        "september" -> Just 9
+        "october"   -> Just 10
+        "november"  -> Just 11
+        "december"  -> Just 12
+        _           -> Nothing
+    }
+    
+readToken (YearLong) ds = do
+    y <- count 4 digit
+    return ds { yearState = Just (read y) }
+
+readToken (Literal s) ds = do
+    string s
+    return ds
+
+readToken _ ds = return ds
+
+parseDate :: [Token] -> String -> Either ParseError (Maybe Date)
+parseDate tks s = 
+    case dateState of
+        Right ds -> case ds of
+            DateState (Just y) (Just m) (Just d) -> Right $ mkDate y m d
+            _                                    -> Right Nothing
+        Left e   -> Left e
+    where initial = return $ DateState { 
+              yearState = Nothing
+              , monthState = Nothing
+              , dayState = Nothing 
+          }
+          pDateState = foldl (>>=) initial (map readToken tks)
+          dateState = parse pDateState "Data.Date" s
 
 data Token =
       DayNumber
@@ -43,7 +116,6 @@ data Token =
     | YearLong
     | Literal String
     deriving Show
-
 
 identifier :: Parser Token
 identifier = do
@@ -77,8 +149,8 @@ tokens = many token
 takeLast :: Int -> [a] -> [a]
 takeLast n xs = drop (length xs - n) xs
 
-parseTokens :: String -> Either ParseError [Token]
-parseTokens = parse tokens "Data.Date.Parsing"
+tokenize :: String -> Either ParseError [Token]
+tokenize = parse tokens "Data.Date.Parsing"
 
 showToken :: Date -> Token -> String
 showToken d t =
